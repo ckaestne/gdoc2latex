641dab143c388466bffdfa30260ecc016c5bdddc
Christian Kaestner <kaestner@cs.cmu.edu>
2022-01-01 10:43:54 -0500

    adding uncommited past changes

--- src/main/scala/GDoc2Latex.scala
+++ src/main/scala/GDoc2Latex.scala
@@ -1,13 +1,14 @@
-import com.google.api.services.docs.v1.model.{Document, Paragraph, ParagraphElement, StructuralElement}
+package edu.cmu.ckaestne.gdoc2latex
+
+import com.google.api.services.docs.v1.model.{Document, Paragraph, ParagraphElement}
 
 import java.io.{File, FileWriter, PrintWriter}
-import scala.collection.mutable
-import scala.jdk.CollectionConverters._
 
 
 object GDoc2Latex extends App {
+  private val DOCUMENT_ID = "1d4yWGqfg9BvWYEHxV2AmiM6DQ-UQyqNLltn59jLx0E8"
 
-  val doc = GDocConnection.getDocument()
+  val doc = GDocConnection.getDocument(DOCUMENT_ID)
   val outw: PrintWriter =
     if (args.size >= 1) new PrintWriter(new FileWriter(new File(args(0))))
     else new PrintWriter(System.out);
@@ -15,130 +16,13 @@ object GDoc2Latex extends App {
     if (args.size == 2) new PrintWriter(new FileWriter(new File(args(1))))
     else new PrintWriter(System.out);
 
-  var _abstract = ""
-  var _title = "title"
 
-  process(outw, outw2, doc);
+  val ldoc = new GDoc2LatexConverter().convert(doc)
+  outw.print(ldoc.latexBody)
+
+  outw2.println(s"\\newcommand\\papertitle{${ldoc.title}}")
+  outw2.println(s"\\newcommand\\paperabstract{${ldoc.abstr}}")
+
   outw.close()
   outw2.close()
-
-
-  def getParagraphText(p: Paragraph): String = if (p.getElements == null || isPaperpileRef(p)) "" else {
-    p.getElements.asScala.flatMap(getParagraphElementText).mkString.trim
-  }
-
-  def getParagraphElementText(e: ParagraphElement): Option[String] = {
-    if (e.getTextRun == null) System.err.println(e)
-    if (e.getTextRun == null || e.getTextRun.getContent == null) None
-    else {
-      var text = processPlainText(e.getTextRun.getContent)
-      val style = e.getTextRun.getTextStyle
-      //use strikethrough as comment
-      if (style != null && style.getStrikethrough)
-        return None
-      if (style != null && style.getBold && text.trim.nonEmpty)
-        text = s"\\textbf{$text}"
-      if (style != null && style.getItalic && text.trim.nonEmpty)
-        text = s"\\emph{$text}"
-      if (style != null && style.getLink != null && text.trim.nonEmpty) {
-        val link = style.getLink.getUrl
-        val hlink = style.getLink.getHeadingId
-        if (hlink != null) {
-          text = s"\\ref{$hlink}"
-        } else if (link != null && (link contains "paperpile.com/c/")) {
-          val refs = link.drop(link.lastIndexOf("/") + 1).split("\\+")
-          text = "\\cite{" + refs.mkString(",") + "}"
-        } else if (link != null && !(link contains "paperpile.com/b/"))
-          System.err.println(s"unsupported direct link ${style.getLink.getUrl} for $text")
-      }
-
-
-      Some(text)
-    }
-  }
-
-  def isPaperpileRef(p: Paragraph): Boolean =
-    if (p.getElements == null) false
-    else p.getElements.asScala.exists(hasPaperpileRefLink)
-
-  private def hasPaperpileRefLink(e: ParagraphElement): Boolean = getPaperpileRefLink(e).isDefined
-
-  private def getPaperpileRefLink(e: ParagraphElement): Option[String] =
-    if (e.getTextRun == null ||
-      e.getTextRun.getTextStyle == null ||
-      e.getTextRun.getTextStyle.getLink == null ||
-      e.getTextRun.getTextStyle.getLink.getUrl == null) None
-    else {
-      val link = e.getTextRun.getTextStyle.getLink.getUrl
-      if (link contains "paperpile.com/b/")
-        Some(link.drop(link.lastIndexOf("/") + 1))
-      else None
-    }
-
-
-  def processPlainText(s: String): String =
-    s.replace("“", "``").replace("”", "''").replace("’", "'").replace("%", "\\%").replace("&", "\\&")
-
-
-  def processParagraph(out: PrintWriter, p: Paragraph): Unit = {
-    if (p.getBullet != null)
-      out.println("\\begin{compactitem}\\item")
-    val text = getParagraphText(p)
-    if (text startsWith "Abstract: ")
-      _abstract = text.drop(10)
-    else
-      out.println(text + "\n")
-    if (p.getBullet != null)
-      out.println("\\end{compactitem}")
-  }
-
-  def printPaperpileRefs(out: PrintWriter, paragraphs: List[Paragraph]): Unit = {
-    out.println("\\begin{thebibliography}{100}")
-    paragraphs.filter(isPaperpileRef).foreach(printPaperpileRef(out, _))
-    out.println("\\end{thebibliography}")
-  }
-
-  def printPaperpileRef(out: PrintWriter, p: Paragraph): Unit = {
-    val f = p.getElements.asScala.filter(hasPaperpileRefLink)
-    if (f.isEmpty) return
-    val id = getPaperpileRefLink(f.head).get
-    val ref = f.flatMap(getParagraphElementText).mkString
-    out.println(s"\\bibitem{$id} $ref")
-  }
-
-  def process(out: PrintWriter,out2: PrintWriter, doc: Document): Unit = {
-    val body = doc.getBody
-
-    val paragraphs = body.getContent.asScala.flatMap(se => Option(se.getParagraph)).toList
-
-    for (p: Paragraph <- paragraphs) {
-      //      println(c)
-      val style = p.getParagraphStyle.getNamedStyleType
-      val ptext = getParagraphText(p)
-      if ("TITLE" == style && ptext.trim.nonEmpty)
-        _title = ptext
-      else if ("HEADING_1" == style && ptext.trim.nonEmpty)
-        out.println(s"\n\\section{${ptext}}\n")
-      else if ("HEADING_2" == style && ptext.trim.nonEmpty)
-        out.println(s"\n\\subsection{${ptext}}\n")
-      else if ("HEADING_3" == style && ptext.trim.nonEmpty)
-        out.println(s"\n\\subsubsection{${ptext}}\n")
-      else if ("NORMAL_TEXT" == style)
-        processParagraph(out, p)
-      else
-        System.err.println("unknonw style: " + style)
-
-      val headingId = p.getParagraphStyle.getHeadingId
-      if (headingId != null)
-        out.println(s"\\label{$headingId}")
-
-
-    }
-    printPaperpileRefs(out, paragraphs)
-
-    out2.println(s"\\newcommand\\papertitle{${_title}}")
-    out2.println(s"\\newcommand\\paperabstract{${_abstract}}")
-
-    //    println(doc)
-  }
 }
--- src/main/scala/GDocConnection.scala
+++ src/main/scala/GDocConnection.scala
@@ -1,3 +1,4 @@
+package edu.cmu.ckaestne.gdoc2latex
 import scala.jdk.CollectionConverters._
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
@@ -15,16 +16,17 @@ import java.util
 import java.util.Collections
 
 object GDocConnection {
-  private val APPLICATION_NAME = "Google Docs API Extract Guide"
+  private val APPLICATION_NAME = "GDoc2Latex"
   private val JSON_FACTORY = JacksonFactory.getDefaultInstance
   private val TOKENS_DIRECTORY_PATH = "tokens"
-  private val DOCUMENT_ID = "12c4SkEdo72PwHWcXLh9l9N5m2IxpaDWK0qCFlG_Fmhg"
   /**
    * Global instance of the scopes required by this quickstart. If modifying these scopes, delete
    * your previously saved tokens/ folder.
    */
   private val SCOPES = Collections.singletonList(DocsScopes.DOCUMENTS_READONLY)
-  private val CREDENTIALS_FILE_PATH = "client_secret_728363515906-qge3rgq2d00r7kv2rliiq9rabm0u94b8.apps.googleusercontent.com.json"
+
+  //this is not considered private for desktop applications and shipped as secret with this app:
+  private val CREDENTIALS_FILE_PATH = "/client_secret_728363515906-0cnss9311fvilo2l4ngorsnl5m49pmso.apps.googleusercontent.com.json"
 
   /**
    * Creates an authorized Credential object.
@@ -43,65 +45,11 @@ object GDocConnection {
     new AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
   }
 
-//  /**
-//   * Returns the text in the given ParagraphElement.
-//   *
-//   * @param element a ParagraphElement from a Google Doc
-//   */
-//  private def readParagraphElement(element: ParagraphElement): String = {
-//    val run = element.getTextRun
-//    if (run == null || run.getContent == null) { // The TextRun can be null if there is an inline object.
-//      return ""
-//    }
-//    run.getContent
-//  }
-//
-//  /**
-//   * Recurses through a list of Structural Elements to read a document's text where text may be in
-//   * nested elements.
-//   *
-//   * @param elements a list of Structural Elements
-//   */
-//  private def readStructuralElements(elements: util.List[StructuralElement]) = {
-//    val sb = new StringBuilder
-//    for (element <- elements.asScala) {
-//      if (element.getParagraph != null) {
-//        for (paragraphElement <- element.getParagraph.getElements.asScala) {
-//          sb.append(readParagraphElement(paragraphElement))
-//        }
-//      }
-//      else if (element.getTable != null) { // The text in table cells are in nested Structural Elements and tables may be
-//        // nested.
-//        for (row <- element.getTable.getTableRows.asScala) {
-//          for (cell <- row.getTableCells.asScala) {
-//            sb.append(readStructuralElements(cell.getContent))
-//          }
-//        }
-//      }
-//      else if (element.getTableOfContents != null) { // The text in the TOC is also in a Structural Element.
-//        sb.append(readStructuralElements(element.getTableOfContents.getContent))
-//      }
-//    }
-//    sb.toString
-//  }
-
-  //    public static void main(String... args) throws IOException, GeneralSecurityException {
-  //        // Build a new authorized API client service.
-  //        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-  //        Docs service =
-  //                new Docs.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-  //                        .setApplicationName(APPLICATION_NAME)
-  //                        .build();
-  //
-  //        Document doc = service.documents().get(DOCUMENT_ID).execute();
-  //        new GDoc2Latex().process(doc);
-  ////        System.out.println(readStructuralElements(doc.getBody().getContent()));
-  //    }
   @throws[IOException]
   @throws[GeneralSecurityException]
-  def getDocument() = {
+  def getDocument(documentId: String) = {
     val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport
     val service = new Docs.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build
-    service.documents.get(DOCUMENT_ID).execute
+    service.documents.get(documentId).execute
   }
 }
