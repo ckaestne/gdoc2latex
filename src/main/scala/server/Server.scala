83ebfcead4fb9b9f60aa8cde3594e3091bf722bb
Christian Kaestner <kaestner@cs.cmu.edu>
2022-12-10 11:38:11 -0500

    fix timeout when output gets too large

--- src/main/scala/server/Server.scala
+++ src/main/scala/server/Server.scala
@@ -106,8 +106,8 @@ object GDoc2LatexWorker {
       Files.write(file, content)
     }
     val startTime = System.currentTimeMillis()
-    val cmd1 = Seq("timeout", "30s", "pdflatex", "-draftmode", "--interaction=nonstopmode", "main.tex")
-    val cmd2 = Seq("timeout", "30s", "pdflatex", "--interaction=nonstopmode", "main.tex")
+    val cmd1 = Seq("timeout", "10s", "pdflatex", "-draftmode", "--interaction=nonstopmode", "main.tex")
+    val cmd2 = Seq("timeout", "10s", "pdflatex", "--interaction=nonstopmode", "main.tex")
     val (exitCode1, out1, err1) = run(cmd1, workingDirectory)
     val midTime = System.currentTimeMillis()
     println(s"pdflatex -draftmode done. ${midTime - startTime} ms, exit code $exitCode1")
@@ -139,10 +139,11 @@ object GDoc2LatexWorker {
       .directory(workingDirectory.toFile).start()
     val out = process.getInputStream
     val err = process.getErrorStream
-    val exitCode = process.waitFor()
 
     val log = new BufferedReader(new InputStreamReader(out)).lines().collect(Collectors.joining("\n"))
     val errorLog = new BufferedReader(new InputStreamReader(err)).lines().collect(Collectors.joining("\n"))
+    val exitCode = process.waitFor()
+
     (exitCode, log, errorLog)
   }
 
