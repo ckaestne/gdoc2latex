52975d57c2f36a9a8f11227af340e099af7e331c
Christian Kaestner <kaestner@cs.cmu.edu>
2024-05-29 15:55:01 -0400

    use fly command rather than heroku by default

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,7 +1,7 @@
 sbt stage
-sudo docker build . -t gdoc
-sudo heroku container:push web
-sudo heroku container:release web
+#sudo docker build . -t gdoc
+#sudo heroku container:push web
+#sudo heroku container:release web
 
 # fly.io is simply, but configure fly.toml file to listen on port 3000 
-# fly launch
+fly deploy
